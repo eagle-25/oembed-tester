6f27d6e5d1beb8fe2160289108760126c738c93f
eagle-25 <seungjun.park025@gmail.com>
2022-12-17 22:00:41 +0900

    feat(oembed.domain): add a feat that replaces {format} to json in endpoint.url
    add a feat that replaces {format} to json in endpoint.url

--- src/oEmbedTester.Domain/oEmbedProvider/Endpoint.cs
+++ src/oEmbedTester.Domain/oEmbedProvider/Endpoint.cs
@@ -2,8 +2,15 @@ namespace oEmbedTester.Domain.oEmbedProvider;
 
 public class EndPoint
 {
+    private readonly string? _url;
+    
     public IEnumerable<string>? Schemes { get; set; }
-    public string? Url { get; set; }
+    public string? Url
+    {
+        get => _url;
+        init => _url = value
+            .Replace("{format}","json");
+    }
     public bool Discovery { get; set; }
     public IEnumerable<string>? Formats { get; set; }
 }
